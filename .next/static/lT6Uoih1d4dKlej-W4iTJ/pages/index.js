476d59847096f4aeabe860ce16cafbf070f72815
Scott Morrison <scott@morrisonlive.ca>
2020-02-25 20:45:11 -0500

    initial commit

---
+++ .next/static/lT6Uoih1d4dKlej-W4iTJ/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{RNiq:function(n,t,o){"use strict";o.r(t),o.d(t,"default",(function(){return i}));var u=o("q1tI"),e=o.n(u).a.createElement;function i(){return e("p",null,"Hello World")}},vlRD:function(n,t,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return o("RNiq")}])}},[["vlRD",1,2,0]]]);
